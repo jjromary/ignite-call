e67751e204ad9d0d6ecea308fa7eb2cfb268c8cc
José Romary Brandão <joseromarybrandao@gmail.com>
2023-02-25 02:14:16 -0300

    create: next oAuth. Autenticação com o Google. Criação de usuário

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,8 +1,17 @@
-import type { AppProps } from 'next/app'
-import { globaStyles } from '../styles/global'
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import { globaStyles } from "../styles/global";
 
-globaStyles()
+globaStyles();
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
+
---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,64 @@
+import { Button, Heading, MultiStep, Text } from "@ignite-ui/react";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { ArrowRight, Check } from "phosphor-react";
+import { Container, Header } from "../styles";
+import { AuthError, ConnectItem, ConnnectBox } from "./styles";
+
+export default function Register() {
+  async function handleConnectCalendar() {
+    signIn("google");
+  }
+
+  const session = useSession();
+  const router = useRouter();
+
+  const hasAuthError = !!router.query.error;
+  const isSignedIn = session.status === "authenticated";
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Conecte seu calendário para verificar automaticamente asd horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          {isSignedIn ? (
+            <Button size="sm" disabled>
+              Conectado
+              <Check />
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={handleConnectCalendar}
+            >
+              Conectar
+              <ArrowRight />
+            </Button>
+          )}
+        </ConnectItem>
+
+        {hasAuthError && (
+          <AuthError size="sm">
+            Falha ao se conectar ao Google, verifque se você habilitou as
+            poermiss~çoes de acesso ao Google Calendar.
+          </AuthError>
+        )}
+
+        <Button type="submit" disabled={!isSignedIn}>
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </ConnnectBox>
+    </Container>
+  );
+}
