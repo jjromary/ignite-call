6c1af762bc37492169fbe210357b561c0dc080e1
José Romary Brandão <joseromarybrandao@gmail.com>
2023-02-28 23:48:10 -0300

    checkbox controllerr

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -1,44 +1,47 @@
 import { Button, Checkbox, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
 import { ArrowRight } from "phosphor-react";
-import { useFieldArray, useForm } from 'react-hook-form';
+import { Controller, useFieldArray, useForm } from 'react-hook-form';
 import { z } from "zod";
 import { getWeekDays } from "../../../utils/get-week-days";
 import { Container, Header } from "../styles";
 import { IntervalBox, IntervalContainer, IntervalDay, IntervalInputs, IntervalItem } from "./styles";
 
 const timeINtervalsFormSchema = z.object({
-  
+
 })
 
 export default function TimeIntervals() {
-  
+
   const {
     register,
     handleSubmit,
     control,
-    formState: {isSubmitting, errors},
+    watch,
+    formState: { isSubmitting, errors },
   } = useForm({
     defaultValues: {
       intervals: [
-        {weekday: 0, enabled: false, startTime: '08:00', endTime: '18:00'},
-        {weekday: 1, enabled: true, startTime: '08:00', endTime: '18:00'},
-        {weekday: 2, enabled: true, startTime: '08:00', endTime: '18:00'},
-        {weekday: 3, enabled: true, startTime: '08:00', endTime: '18:00'},
-        {weekday: 4, enabled: true, startTime: '08:00', endTime: '18:00'},
-        {weekday: 5, enabled: true, startTime: '08:00', endTime: '18:00'},
-        {weekday: 6, enabled: false, startTime: '08:00', endTime: '18:00'},
+        { weekday: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
+        { weekday: 1, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekday: 2, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekday: 3, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekday: 4, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekday: 5, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekday: 6, enabled: false, startTime: '08:00', endTime: '18:00' },
       ]
     }
   })
 
   const weekDays = getWeekDays()
 
-  const {fields} = useFieldArray({
+  const { fields } = useFieldArray({
     name: 'intervals',
     control,
   })
 
-  async function handleSetTimeIntervlas() {}
+  const intervals = watch('intervals')
+
+  async function handleSetTimeIntervlas() { }
 
   return (
     <Container>
@@ -52,26 +55,39 @@ export default function TimeIntervals() {
 
       <IntervalBox as="form" onSubmit={handleSubmit(handleSetTimeIntervlas)}>
         <IntervalContainer>
-         {fields.map((field, index) => {
-          return (
-            <IntervalItem key={field.id}>
-              <IntervalDay>
-                <Checkbox />
-                <Text>{weekDays[field.weekday]}</Text>
-              </IntervalDay>
+          {fields.map((field, index) => {
+            return (
+              <IntervalItem key={field.id}>
+                <IntervalDay>
+                  <Controller
+                    name={`intervals.${index}.enabled`}
+                    control={control}
+                    render={({ field }) => {
+                      return (
+                        <Checkbox
+                          onCheckedChange={checked => {
+                            field.onChange(checked === true)
+                          }}
+                          checked={field.value}
+                        />
+                      )
+                    }}
+                  />
+                  <Text>{weekDays[field.weekday]}</Text>
+                </IntervalDay>
                 <IntervalInputs>
-                <TextInput size='sm' type='time' step={60} {...register(`intervals.${index}.startTime`)}/>
-                <TextInput size='sm' type='time' step={60} {...register(`intervals.${index}.endTime`)}/>
-              </IntervalInputs>
-          </IntervalItem>
-          )
-         })}
-         
+                  <TextInput size='sm' type='time' step={60} disabled={intervals[index].enabled === false} {...register(`intervals.${index}.startTime`)} />
+                  <TextInput size='sm' type='time' step={60} disabled={intervals[index].enabled === false} {...register(`intervals.${index}.endTime`)} />
+                </IntervalInputs>
+              </IntervalItem>
+            )
+          })}
+
         </IntervalContainer>
-      
-      <Button type='submit'>Próximo passo <ArrowRight/></Button>
+
+        <Button type='submit'>Próximo passo <ArrowRight /></Button>
       </IntervalBox>
-      
+
     </Container>
   );
 }
