650ed4d3f16f67a5e2f2fcc502c151fbb01da4e1
José Romary Brandão <joseromarybrandao@gmail.com>
2023-03-10 00:05:17 -0300

    Estrutura visual da lista de horários

--- src/Components/Calendar/index.tsx
+++ src/Components/Calendar/index.tsx
@@ -37,9 +37,45 @@ export function Calendar() {
             <td></td>
             <td></td>
             <td></td>
-            <td><CalendarDay disabled>01</CalendarDay></td>
+            <td><CalendarDay>01</CalendarDay></td>
             <td><CalendarDay>02</CalendarDay></td>
-            <td><CalendarDay>03</CalendarDay></td>
+            <td><CalendarDay disabled>03</CalendarDay></td>
+          </tr>
+          <tr>
+            <td><CalendarDay disabled>04</CalendarDay></td>
+            <td><CalendarDay>05</CalendarDay></td>
+            <td><CalendarDay>06</CalendarDay></td>
+            <td><CalendarDay>07</CalendarDay></td>
+            <td><CalendarDay>08</CalendarDay></td>
+            <td><CalendarDay>09</CalendarDay></td>
+            <td><CalendarDay disabled>10</CalendarDay></td>
+          </tr>
+          <tr>
+            <td><CalendarDay disabled>11</CalendarDay></td>
+            <td><CalendarDay>12</CalendarDay></td>
+            <td><CalendarDay>13</CalendarDay></td>
+            <td><CalendarDay>14</CalendarDay></td>
+            <td><CalendarDay>15</CalendarDay></td>
+            <td><CalendarDay>16</CalendarDay></td>
+            <td><CalendarDay disabled>17</CalendarDay></td>
+          </tr>
+          <tr>
+            <td><CalendarDay disabled>18</CalendarDay></td>
+            <td><CalendarDay>19</CalendarDay></td>
+            <td><CalendarDay>20</CalendarDay></td>
+            <td><CalendarDay>21</CalendarDay></td>
+            <td><CalendarDay>22</CalendarDay></td>
+            <td><CalendarDay>23</CalendarDay></td>
+            <td><CalendarDay disabled>24</CalendarDay></td>
+          </tr>
+          <tr>
+            <td><CalendarDay disabled>25</CalendarDay></td>
+            <td><CalendarDay>26</CalendarDay></td>
+            <td><CalendarDay>27</CalendarDay></td>
+            <td><CalendarDay>28</CalendarDay></td>
+            <td><CalendarDay>29</CalendarDay></td>
+            <td><CalendarDay>30</CalendarDay></td>
+            <td><CalendarDay disabled>31</CalendarDay></td>
           </tr>
         </tbody>
       </CalendarBody>
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,10 +1,43 @@
 import { Calendar } from "../../../../../Components/Calendar";
-import { Container } from "./styles";
+import { Container, TimePicker, TimePickerHeader, TimePickerItem, TimePickerList } from "./styles";
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected &&
+        <TimePicker>
+          <TimePickerHeader>
+            Sabado-Feira <span>30 de Fevereiro</span>
+          </TimePickerHeader>
+          <TimePickerList>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+            <TimePickerItem>25:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      }
+
     </Container>
 
   )
