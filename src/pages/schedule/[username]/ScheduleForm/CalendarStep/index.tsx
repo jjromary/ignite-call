49d6cbb0a81c0be12290f285502d7523bd8bcca3
José Romary Brandão <joseromarybrandao@gmail.com>
2023-03-16 11:06:40 -0300

    Preparando calendário

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import { Calendar } from "../../../../../Components/Calendar";
 import { Container, TimePicker, TimePickerHeader, TimePickerItem, TimePickerList } from "./styles";
 
 export function CalendarStep() {
-  const isDateSelected = false
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+
+  const isDateSelected = !!selectedDate
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
-      <Calendar />
+      <Calendar
+        selectedDate={selectedDate}
+        onDateSelected={setSelectedDate}
+      />
 
       {isDateSelected &&
         <TimePicker>
